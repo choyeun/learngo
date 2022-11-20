c6b549fdf2c157a57cc40d1f54b599160644c424
choyeun <choyeun0372@gmail.com>
2022-11-20 15:53:41 +0900

    .

--- main.go
+++ main.go
@@ -9,6 +9,11 @@ func main() {
 	account := accounts.NewAccount("choyeun")
 	account.Deposit(10)
 	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.Balance())
 }
 
 /**
